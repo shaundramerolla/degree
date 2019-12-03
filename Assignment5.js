b9514bc84ed1ddb2cfca9ed70aa6f7950d62726c
shaundramerolla <58342737+shaundramerolla@users.noreply.github.com>
2019-12-02 20:27:24 -0500

    Update Assignment5.js

--- Assignment5.js
+++ Assignment5.js
@@ -4,25 +4,9 @@ const button = document.getElementById("button");
         xhr.onload = function () {
             if(xhr.status === 200) {
                 responseObject = JSON.parse(xhr.responseText);
-                var degreesContent = '';
-                for (var i=0; i<responseObject.my_degrees.length; i++) {
-                    degreesContent +='<thead>';
-                    degreesContent +='<tr>School</tr>'
-                        +responseObject.my_degrees[i].school
-                        + ' ';
-                    degreesContent +='<tr>Major</tr>'
-                        +responseObject.my_degrees[i].major
-                        + ' ';
-                    degreesContent +='<tr>Type</tr>'
-                        +responseObject.my_degrees[i].type
-                        + ' ';
-                    degreesContent +='<tr>Graduation</tr>'
-                        +responseObject.my_degrees[i].graduation
-                        + '<br><br>';
                 }
-                document.getElementById("table").innerHTML = degreesContent;
+                document.write(responseObject);
             }
         };
         xhr.open('GET', 'Assignment5.json', true);
         xhr.send(null);
-    }
